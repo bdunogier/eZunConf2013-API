37387d252eff781e8a5f8626d62f64ce981ab5c3
Bertrand Dunogier <bd@ez.no>
2013-05-25 15:21:31 +0200

    Implemented image importer stage 2
    Imports a recursive folder/image structure to a Folder

--- Command/ImageImportCommand.php
+++ Command/ImageImportCommand.php
@@ -1,11 +1,12 @@
 <?php
 namespace eZ\UnconBundle\Command;
 
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
-    Symfony\Component\Console\Input\InputInterface,
-    Symfony\Component\Console\Output\OutputInterface,
-    Symfony\Component\Console\Input\InputArgument,
-    Symfony\Component\Console\Input\InputOption;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use eZ\Publish\API\Repository\Values\Content\LocationCreateStruct;
 
 class ImageImportCommand extends ContainerAwareCommand
 {
@@ -21,7 +22,7 @@ class ImageImportCommand extends ContainerAwareCommand
 
     protected function execute( InputInterface $input, OutputInterface $output )
     {
-        $sourcePath = $input->getArgument( 'source-path' );
+        $sourcePath = realpath( $input->getArgument( 'source-path' ) );
         if ( !file_exists( $sourcePath ) )
         {
             $output->writeln( "Source path '$sourcePath'  doesn\'t exist" );
@@ -33,8 +34,7 @@ class ImageImportCommand extends ContainerAwareCommand
             exit( 1 );
         }
 
-        $repository = $this->getContainer()->get( 'ezpublish.api.repository' );
-        $repository->setCurrentUser( $repository->getUserService()->loadUser( 14 ) );
+        $this->logInAsAdmin();
 
         $targetLocationId = $input->getArgument( 'target-location-id' );
         $locationService = $this->getContainer()->get( 'ezpublish.api.repository' )->getLocationService();
@@ -54,36 +54,138 @@ class ImageImportCommand extends ContainerAwareCommand
             exit( 1 );
         }
 
-        $imageContentType = $this->getContainer()->get( 'ezpublish.api.repository' )->getContentTypeService()->loadContentTypeByIdentifier( 'image' );
+        $this->createAndPublishStructure(
+            new \RecursiveDirectoryIterator( $sourcePath, \FilesystemIterator::CURRENT_AS_FILEINFO|\FilesystemIterator::SKIP_DOTS ),
+            $locationService->loadLocation( $targetLocationId ),
+            $output
+        );
+    }
 
-        $contentService = $this->getContainer()->get( 'ezpublish.api.repository' )->getContentService();
-
-        $locationCreateStruct = $locationService->newLocationCreateStruct( $targetLocationId );
-
-        /** @var $file SplFileInfo */
-        foreach ( new \DirectoryIterator( $sourcePath ) as $file )
+    public function createAndPublishStructure( \RecursiveDirectoryIterator $iterator, \eZ\Publish\API\Repository\Values\Content\Location $parentLocation, OutputInterface $output )
+    {
+        /** @var $file \SplFileInfo */
+        foreach ( $iterator as $file )
         {
-            if ( !$file->isFile() || !in_array( $file->getExtension(), array( 'jpg', 'png', 'gif' ) ) )
+            if ( $iterator->hasChildren() )
+            {
+                $output->writeln( "Entering folder " . $file->getRealPath() );
+
+                // Create the folder
+                $folder = $this->createAndPublishFolder( $file->getBasename(), $parentLocation->id );
+                $output->writeln( "Created folder '" . $folder->contentInfo->name . "'" );
+
+                // Dive in
+                $this->createAndPublishStructure( $iterator->getChildren(), $folder, $output );
+            }
+
+            if ( !in_array( strtolower( $file->getExtension() ), array( 'jpg', 'gif', 'png' ) ) )
                 continue;
 
-            $imageCreateStruct = $contentService->newContentCreateStruct(
-                $imageContentType, 'eng-GB'
-            );
+            $image = $this->createAndPublishImage( $file, $parentLocation->id );
+            $output->writeln( "Created image '" . $image->contentInfo->name . "'" );
+        }
+    }
 
-            $nameString = $file->getBasename( '.' . $file->getExtension() );
-            $imageCreateStruct->setField( 'name', $nameString );
-            $imageCreateStruct->setField( 'image', new \eZ\Publish\Core\FieldType\Image\Value(
+    /**
+     * @param string $file
+     * @param int $parentLocationId
+     *
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     */
+    protected function createAndPublishImage( $file, $parentLocationId )
+    {
+        $locationCreateStruct = $this->getRepository()->getLocationService()->newLocationCreateStruct( $parentLocationId );
+        $imageCreateStruct = $this->getContentService()->newContentCreateStruct(
+            $this->getContentType( 'image' ), 'eng-GB'
+        );
+
+        $nameString = $file->getBasename( '.' . $file->getExtension() );
+        $imageCreateStruct->setField( 'name', $nameString );
+        $imageCreateStruct->setField(
+            'image',
+            new \eZ\Publish\Core\FieldType\Image\Value(
                 array(
                     'path' => $file->getRealPath(),
                     'fileSize' => $file->getSize(),
-                    'fileName' =>$file->getBasename(),
+                    'fileName' => $file->getBasename(),
                     'alternativeText' => $nameString
                 )
-            ) );
+            )
+        );
 
-            $imageContent = $contentService->createContent( $imageCreateStruct, array( clone $locationCreateStruct ) );
-            $contentService->publishVersion( $imageContent->versionInfo );
-            $output->writeln( "published image $nameString with id #" . $imageContent->id );
-        }
+        $imageDraft = $this->getContentService()->createContent( $imageCreateStruct, array( $locationCreateStruct ) );
+        $imageContent = $this->getContentService()->publishVersion( $imageDraft->versionInfo );
+
+        return $imageContent;
     }
+
+    /**
+     * Creates and publishes a folder content
+     * @param string $name
+     * @param \eZ\Publish\API\Repository\Values\Content\Location $location
+     * @return \eZ\Publish\API\Repository\Values\Content\Location
+     */
+    protected function createAndPublishFolder( $name, $parentLocationId )
+    {
+        $locationService = $this->getRepository()->getLocationService();
+
+        $locationCreateStruct = $locationService->newLocationCreateStruct( $parentLocationId );
+        $imageCreateStruct = $this->getContentService()->newContentCreateStruct(
+            $this->getContentType( 'folder' ), 'eng-GB'
+        );
+
+        $imageCreateStruct->setField( 'name', $name );
+        $imageDraft = $this->getContentService()->createContent( $imageCreateStruct, array( $locationCreateStruct ) );
+        $imageContent = $this->getContentService()->publishVersion( $imageDraft->versionInfo );
+
+        return $locationService->loadLocation( $imageContent->contentInfo->mainLocationId );
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\Repository
+     */
+    protected function getRepository()
+    {
+        if ( !isset( $this->repository ) )
+        {
+            $this->repository = $this->getContainer()->get( 'ezpublish.api.repository' );
+        }
+        return $this->repository;
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\ContentService
+     */
+    protected function getContentService()
+    {
+        if ( !isset( $this->contentService ) )
+        {
+            $this->contentService = $this->getRepository()->getContentService();
+        }
+        return $this->contentService;
+    }
+
+    /**
+     * @return \eZ\Publish\API\Repository\Values\ContentType\ContentType
+     */
+    protected function getContentType( $contentTypeIdentifier )
+    {
+        return $this->imageContentType = $this->getRepository()
+            ->getContentTypeService()
+            ->loadContentTypeByIdentifier( $contentTypeIdentifier );
+    }
+
+    protected function logInAsAdmin()
+    {
+        $this->getRepository()->setCurrentUser( $this->getRepository()->getUserService()->loadUser( 14 ) );
+    }
+
+    /** @var \eZ\Publish\API\Repository\Repository */
+    protected $repository;
+
+    /** @var \eZ\Publish\API\Repository\ContentService */
+    protected $contentService;
+
+    /** @var \eZ\Publish\API\Repository\Values\ContentType\ContentType */
+    protected $imageContentType;
 }
